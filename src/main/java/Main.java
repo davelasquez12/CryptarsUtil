e19d91326b567ba853da591061330abe9dfb79b2
David <davelasquez12@gmail.com>
2020-08-25 19:22:45 -0500

    Added functionality to update table in DynamoDB with the asset icon url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String,String> iconUrlMap = GetIconsService.executeService();
-
-        System.out.println(iconUrlMap);
+        UpdateIcons.updateAllSupportedIcons();
     }
 }
--- src/main/java/UpdateIcons.java
+++ src/main/java/UpdateIcons.java
@@ -1,12 +1,86 @@
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeAction;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UpdateIcons {
-    public static final String test = "test";
-    public void execute(Map<String, String> iconUrlMapData) {
-        //get list of supported assets from CurrentCryptoPrices table in DynamoDB
+    public static final String TABLE_NAME = "CurrentCryptoPrices";
+    private static DynamoDbClient dynamoDbClient;
 
-        //compare list with the iconUrlsMapData to record the asset icons that are supported
+    private static void initDynamoDbClient() {
+        if(dynamoDbClient == null) {
+            dynamoDbClient = DynamoDbClient.builder()
+                    .region(Region.US_EAST_2)
+                    .credentialsProvider(ProfileCredentialsProvider.create())
+                    .build();
+        }
+    }
 
-        //store recorded urls to the CurrentCryptoPrices table in a new column "assetIcon"
+    public static void updateAllSupportedIcons() {
+        initDynamoDbClient();
+        Map<String,String> iconUrlMap = GetIconsService.executeService();
+
+        //Get full list of supported assets from CurrentCryptoPrices table in DynamoDB
+        List<Map<String, AttributeValue>> assetList = getAssetListFromDynamoDb();
+
+        //Compare assetList with the iconUrlsMap to record the supported asset icons to DynamoDB
+        if(assetList != null && !assetList.isEmpty()) {
+            updateDbForMatchingAssets(iconUrlMap, assetList);
+        }
+    }
+
+    private static List<Map<String, AttributeValue>> getAssetListFromDynamoDb() {
+        ScanRequest scanRequest = ScanRequest.builder().tableName(TABLE_NAME).attributesToGet("AssetId").build();
+        ScanResponse response = dynamoDbClient.scan(scanRequest);
+
+        if(response.sdkHttpResponse().isSuccessful() && response.count() > 0) {
+            return response.items();
+        }
+
+        return null;
+    }
+
+    private static void updateDbForMatchingAssets(Map<String, String> iconUrlMap, List<Map<String, AttributeValue>> dbAssetList) {
+        UpdateItemRequest updateItemRequest = UpdateItemRequest.builder().tableName(TABLE_NAME).build();
+
+        for(Map<String, AttributeValue> asset : dbAssetList) {
+            String assetName = asset.get("AssetId").s().toUpperCase();
+            String iconUrl = iconUrlMap.get(assetName);
+
+            if(iconUrl != null) {
+                //store recorded urls to the CurrentCryptoPrices table in a new column "AssetIconUrl"
+                updateItemToDb(assetName, iconUrl, updateItemRequest);
+            }
+        }
+    }
+
+    private static void updateItemToDb(String assetName, String url, UpdateItemRequest updateItemRequest) {
+        HashMap<String, AttributeValue> primaryKeyItem = new HashMap<>(2);
+        primaryKeyItem.put("AssetId", AttributeValue.builder().s(assetName).build());
+
+        HashMap<String, AttributeValueUpdate> iconUrlUpdate = new HashMap<>(2);
+        AttributeValue attributeValue = AttributeValue.builder().s(url).build();
+        iconUrlUpdate.put("AssetIconUrl", AttributeValueUpdate.builder().value(attributeValue).action(AttributeAction.PUT).build());
+
+        updateItemRequest = updateItemRequest
+                .toBuilder()
+                .key(primaryKeyItem)
+                .attributeUpdates(iconUrlUpdate)
+                .build();
+
+        dynamoDbClient.updateItem(updateItemRequest);
+    }
+
+    public static void updateIconUrlByAssetName(String assetName, String iconUrl) {
+
     }
 }
